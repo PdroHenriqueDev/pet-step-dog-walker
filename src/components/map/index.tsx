4e76493434c91865f6a7b09e7f6b4b238dbed089
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2024-05-13 11:27:04 -0300

    feat: track marker

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,32 +1,107 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import React, { useEffect, useState } from 'react';
+import { View, Platform } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Region } from 'react-native-maps';
 import  styles  from './styles';
 import { Marker } from 'react-native-maps';
+import { PlataformEnum } from '../../enum/platform.enum';
+import { request, PERMISSIONS } from 'react-native-permissions';
+import GetLocation from 'react-native-get-location'
 
 
-const region = {
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.015,
-    longitudeDelta: 0.0121,
-}
-   
 function Map(): React.JSX.Element {
-   
+  const [region, setRegion] = useState<Region>({
+    latitude: -10.1861,
+    longitude: -48.31886,
+    latitudeDelta: 0.005,
+    longitudeDelta: 0.005,
+  });
+
+  const [markerRegion, setMarkerRegion] = useState<Region>({
+    latitude: -10.1861,
+    longitude: -48.31886,
+    latitudeDelta: 0.005,
+    longitudeDelta: 0.005,
+  });
+
+  const requestLocationPermission = async () => {
+    const requestResponse = await request(
+      Platform.OS === PlataformEnum.IOS
+        ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+        : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
+    );
+
+    if (requestResponse === 'granted') {
+      await getLocationAndUpdateRegion();
+
+      const intervalId = setInterval(async () => {
+        await updateMarkerRegion();
+      }, 8000);
+
+      return () => {
+        clearInterval(intervalId);
+      };
+    }
+  }
+
+  const getLocationAndUpdateRegion = async () => {
+    await GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 4000,
+    })
+    .then(location => {
+      const { latitude, longitude } = location;
+      const newRegion = {
+        latitude,
+        longitude,
+        latitudeDelta: region.latitudeDelta,
+        longitudeDelta: region.longitudeDelta,
+      }
+      setRegion(newRegion);      
+    })
+    .catch(error => {
+        const { code, message } = error;
+        console.warn(code, message);
+    })
+  };
+
+  const updateMarkerRegion = async () => { 
+    await GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 4000,
+    })
+    .then(location => {
+      const { latitude, longitude } = location;
+      const newRegion = {
+        latitude,
+        longitude,
+        latitudeDelta: region.latitudeDelta,
+        longitudeDelta: region.longitudeDelta,
+      }
+      setMarkerRegion(newRegion);      
+    })
+    .catch(error => {
+        const { code, message } = error;
+        console.warn(code, message);
+    })
+  }
+
+  useEffect(() => {
+    requestLocationPermission();   
+  }, []);
 
   return (
     <View style={styles.container}>
-        <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+      <MapView
+        provider={PROVIDER_GOOGLE}
         style={styles.map}
         region={region}
-        >
-            <Marker
-                coordinate={{latitude: region.latitude, longitude: region.longitude}}
-                
-            />
-        </MapView>
+        showsUserLocation={true}
+        zoomControlEnabled={true}
+        zoomEnabled={true}
+      >
+        <Marker
+          coordinate={{ latitude: markerRegion.latitude, longitude: markerRegion.longitude }} />
+      </MapView>
     </View>
   );
 }
