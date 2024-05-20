dc4180380b8fcdcfd9acf0f9a8afbdf1a435cef4
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2024-05-20 19:52:43 -0300

    feat: share current location

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,14 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { View, Platform } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Region } from 'react-native-maps';
 import  styles  from './styles';
-import { Marker } from 'react-native-maps';
 import { PlataformEnum } from '../../enum/platform.enum';
 import { request, PERMISSIONS } from 'react-native-permissions';
-import GetLocation from 'react-native-get-location'
+import GetLocation from 'react-native-get-location';
+import { io } from "socket.io-client";
+import ReactNativeForegroundService from "@supersami/rn-foreground-service";
+import BackgroundTimer from 'react-native-background-timer';
 
+const socket = io('http://192.168.0.13:3000', {
+    query: { user_id: '1' }
+});
 
 function Map(): React.JSX.Element {
+  const mapRef = useRef<MapView>(null);
+
   const [region, setRegion] = useState<Region>({
     latitude: -10.1861,
     longitude: -48.31886,
@@ -16,92 +23,114 @@ function Map(): React.JSX.Element {
     longitudeDelta: 0.005,
   });
 
-  const [markerRegion, setMarkerRegion] = useState<Region>({
-    latitude: -10.1861,
-    longitude: -48.31886,
-    latitudeDelta: 0.005,
-    longitudeDelta: 0.005,
-  });
+  const [isRequestingLocation, setIsRequestingLocation] = useState(false);
+
+  useEffect(() => {
+    socket.on('connection', () => {});
+    requestLocationPermission();  
+    getLocationAndUpdateRegion();
+  }, []);
 
   const requestLocationPermission = async () => {
     const requestResponse = await request(
       Platform.OS === PlataformEnum.IOS
-        ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
-        : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
+        ? PERMISSIONS.IOS.LOCATION_ALWAYS
+        : PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION
     );
 
     if (requestResponse === 'granted') {
       await getLocationAndUpdateRegion();
-
-      const intervalId = setInterval(async () => {
-        await updateMarkerRegion();
-      }, 8000);
-
-      return () => {
-        clearInterval(intervalId);
-      };
+      startForegroundService();
+      startBackgroundTimer();
     }
   }
 
   const getLocationAndUpdateRegion = async () => {
-    await GetLocation.getCurrentPosition({
-      enableHighAccuracy: true,
-      timeout: 4000,
-    })
-    .then(location => {
+    if (isRequestingLocation) return;
+
+    setIsRequestingLocation(true);
+    try {
+      const location = await GetLocation.getCurrentPosition({
+        enableHighAccuracy: true,
+        timeout: 60000,
+      });
+
       const { latitude, longitude } = location;
       const newRegion = {
         latitude,
         longitude,
         latitudeDelta: region.latitudeDelta,
         longitudeDelta: region.longitudeDelta,
-      }
-      setRegion(newRegion);      
-    })
-    .catch(error => {
-        const { code, message } = error;
-        console.warn(code, message);
-    })
+      };
+      setRegion(newRegion);
+    } catch (error) {
+      console.warn(error);
+    } finally {
+      setIsRequestingLocation(false);
+    }
   };
 
-  const updateMarkerRegion = async () => { 
-    await GetLocation.getCurrentPosition({
-      enableHighAccuracy: true,
-      timeout: 4000,
-    })
-    .then(location => {
+  const sendLocationToServer  = async () => { 
+    if (isRequestingLocation) return;
+
+    setIsRequestingLocation(true);
+    try {
+      const location = await GetLocation.getCurrentPosition({
+        enableHighAccuracy: true,
+        timeout: 7000,
+      });
+
       const { latitude, longitude } = location;
-      const newRegion = {
-        latitude,
-        longitude,
-        latitudeDelta: region.latitudeDelta,
-        longitudeDelta: region.longitudeDelta,
-      }
-      setMarkerRegion(newRegion);      
-    })
-    .catch(error => {
-        const { code, message } = error;
-        console.warn(code, message);
-    })
+
+      socket.emit('location', { latitude, longitude });
+
+      mapRef.current?.animateCamera({
+        center: {
+          latitude,
+          longitude,
+        }
+      });
+    } catch (error) {
+      console.warn(error);
+    } finally {
+      setIsRequestingLocation(false);
+    }
   }
 
-  useEffect(() => {
-    requestLocationPermission();   
-  }, []);
+  const startForegroundService = () => {
+    ReactNativeForegroundService.add_task(async () => await sendLocationToServer (), {
+      delay: 8000,
+      onLoop: true,
+      taskId: "location_task",
+    });
+
+    ReactNativeForegroundService.start({
+      id: 1244,
+      title: "Map",
+      message: "Updating location",
+      icon: "ic_launcher",
+      button: false,
+      color: "#000000",
+    });
+  }
+
+  const startBackgroundTimer = () => {
+    BackgroundTimer.runBackgroundTimer(() => {
+      startForegroundService();
+    }, 8000);
+  };
 
   return (
     <View style={styles.container}>
       <MapView
+        ref={mapRef}
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         region={region}
         showsUserLocation={true}
         zoomControlEnabled={true}
         zoomEnabled={true}
-      >
-        <Marker
-          coordinate={{ latitude: markerRegion.latitude, longitude: markerRegion.longitude }} />
-      </MapView>
+      />
     </View>
   );
 }
